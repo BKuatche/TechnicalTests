599835aa62ac604f0c45c25e44607f3e7d80aeaa
Berthe Kuatche <epiphanie.berthe32@gmail.com>
2023-07-18 13:44:09 +0100

    Refactor code

--- OhceTechnicalTests/OhceApp/Program.cs
+++ OhceTechnicalTests/OhceApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OhceApp.Services;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string userName = args[0];
+        string stopContent = "Stop!";
+        int hour = DateTime.Now.Hour;
+
+        var ohce = new OhceService(stopContent, hour);
+        ohce.RunApp(userName);
+    }
+}
